fdfc45c248e366a57edfe30dd2d411b70a0d3f92
vipinmehra535 <vipinsinghmehra535@gmail.com>
2025-08-23 23:18:45 +0530

    Binary Search with Recursion

---
+++ binary_search_recursion.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(const vector<int> &arr, int left, int right, int x)
+{
+    if (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == x)
+            return mid;
+        if (arr[mid] > x)
+            return binarySearch(arr, left, mid - 1, x);
+        return binarySearch(arr, mid + 1, right, x);
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {2, 3, 4, 10, 40};
+    int x = 10;
+    int result = binarySearch(arr, 0, arr.size() - 1, x);
+    if (result != -1)
+    {
+        cout << "Element found at index " << result << endl;
+    }
+    else
+    {
+        cout << "Element not found" << endl;
+    }
+    return 0;
+}
